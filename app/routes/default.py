ad22002ff9e8fc55d6d4a64fce057e09c9570107
Melissa Campos <s_melissa.campos@ousd.org>
2022-04-15 12:47:34 -0700

    main

--- app/routes/default.py
+++ app/routes/default.py
@@ -9,3 +9,31 @@ def index():
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+
+@app.route('/African')
+def African():
+    return render_template('African.html')
+
+@app.route('/Asian')
+def Asian():
+    return render_template('Asian.html')
+
+@app.route('/Caribbean')
+def Caribbean():
+    return render_template('Caribbean.html')
+
+@app.route('/European')
+def European():
+    return render_template('European.html')
+
+@app.route('/Latin')
+def Latin():
+    return render_template('Latin.html')
+
+@app.route('/North')
+def North():
+    return render_template('North.html')
+
+@app.route('/Top')
+def Top():
+    return render_template('Top.html')
