f202d4091592852f31d8396d013a8a033e19a38a
Melissa Campos <s_melissa.campos@ousd.org>
2022-04-19 13:28:35 -0700

    mater

--- app/routes/default.py
+++ app/routes/default.py
@@ -14,6 +14,67 @@ def aboutus():
 def African():
     return render_template('African.html')
 
+@app.route('/Afrobeats')
+def Afrobeats():
+    return render_template('Afrobeats.html')
+@app.route('/Benga')
+def Benga():
+    return render_template('Benga.html')
+@app.route('/Chimurenga')
+def Chimurenga():
+    return render_template('Chimurenga.html')
+@app.route('/Ethio')
+def Ethio():
+    return render_template('Ethio.html')
+@app.route('/Gnawa')
+def Gnawa():
+    return render_template('Highlife.html')
+@app.route('/Highlife')
+def Highlife():
+    return render_template('Highlife.html')
+@app.route('/Hiplife')
+def Hiplife():
+    return render_template('Hiplife.html')
+@app.route('/Inkiranya')
+def Inkiranya():
+    return render_template('Inkiraya.html')
+@app.route('/Juju')
+def Juju():
+    return render_template('juju.html')
+@app.route('/Majika')
+def Majika():
+    return render_template('Majika.html')
+@app.route('/Mbalax')
+def Mbalax():
+    return render_template('mbalax.html')
+@app.route('/Ndombolo')
+def Ndombolo():
+    return render_template('Ndombolo.html')
+@app.route('/Palmwine')
+def Palmwine():
+    return render_template('palmwine.html')
+@app.route('/Rababah')
+def Rababah():
+    return render_template('Rababah.html')
+@app.route('/Shaabi')
+def Shaabi():
+    return render_template('Shaabi.html')
+@app.route('/somalijazz')
+def somalijazz():
+    return render_template('somalijazz.html')
+@app.route('/soukou')
+def soukou():
+    return render_template('soukou.html')
+@app.route('/ubongo')
+def ubongo():
+    return render_template('ubongo.html')
+@app.route('/zilin')
+def zilin():
+    return render_template('zilin.html')
+@app.route('/zouglou')
+def zouglou():
+    return render_template('zouglou.html')
+
 @app.route('/Asian')
 def Asian():
     return render_template('Asian.html')
